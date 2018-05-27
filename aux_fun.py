020d08a364dc175af3a04cc21ab36beb3a52d06c
Unknown <giada.confortola@mail.polimi.it>
2018-05-27 20:00:04 +0200

    Random forest notebook with code to save data in json

--- aux_fun.py
+++ aux_fun.py
@@ -2,15 +2,13 @@ import pandas as pd
 import numpy as np
 import datetime
 import time
+import json
 
 #takes in input the dataset used as testSet and the datset with results
 def evaluate(test, result):
-
     merged = merge_for_evaluation(test, result)
     num = (np.abs(merged['NumberOfSales_x']- merged['NumberOfSales_y'])).groupby(test['Region']).sum()
-    #print(num)
     den = merged['NumberOfSales_x'].groupby(test['Region']).sum()
-    #print(den)
     err_region = np.array(num/den)
     n_regions = len(merged['Region'].unique())
     err = np.sum(err_region, axis=0)/n_regions
